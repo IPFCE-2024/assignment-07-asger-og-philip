1e647bb2b0d39214949ce7b10a063e3aba9db96c
asgernh <asgernhspil123@gmail.com>
2025-10-09 16:32:15 +0200

    Opgave 1 er done, tests er fucked

--- exercise1.c
+++ exercise1.c
@@ -7,8 +7,39 @@
  * 
  * Taylor series for sin(x) = x - x^3/3! + x^5/5! - x^7/7! + ...
  */
+ 
+//#include "taylor_sine.h"
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
 
-#include "taylor_sine.h"
+/* Compute the factorial of a given number. */
+int fact(int n)
+{
+    int f;
+    int g; /* previous factorial */
+    int i; /* counter variable for factorial */
+    int j; /* counter variable for multiplication */
+    /* precondition */
+    assert(n >= 0);
+
+    /* postcondition */
+    g = 1;
+    f = 1;
+    for (i = 0; i < n; i = i + 1)
+    { /* invariant: f equals factorial of i
+         *            0 <= i <= n
+         */
+        for (j = 0; j < i; j = j + 1)
+        { /* invariant: g = (j + 1) * f
+           *            0 <= j <= i 
+           */
+            g = g + f;
+        }
+        f = g;
+    }
+return f;
+}
 
 /* 
  * Calculate sine using Taylor series approximation
@@ -20,6 +51,17 @@ double taylor_sine(double x, int n) {
     // TODO: Implement the Taylor series approximation for sine
     // Hint: The series is: x - x^3/3! + x^5/5! - x^7/7! + ...
     // Use a loop to calculate n terms of the series
-    
-    return 0.0; // placeholder - replace with your implementation
+    double svar=0;
+    int skifter=1;
+    int i2=1;
+    for(int i=0;i<n;i++){
+        double place=pow(x,i2)/fact(i2);
+        svar=svar+(skifter*place);
+        skifter=skifter*-1;
+        i2=i2+2;
+    }
+    return svar; // placeholder - replace with your implementation
 }
+//int main(void){
+  //  printf("\n%lf",taylor_sine(6.0,5));
+//}
